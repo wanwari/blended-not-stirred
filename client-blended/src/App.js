03b2a429002a5e3291eaa0d95ec7da5ed959eeda
Wiesa Anwari <contact@wanwari.ca>
2020-05-29 20:44:52 -0400

    setup base client

---
+++ client-blended/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Recipe from './Components/Recipe/Recipe';
+
+function App() {
+  return (
+    <div>
+        <Recipe />
+    </div>
+  );
+}
+
+export default App;
---
+++ client-blended/src/Components/Recipe/Recipe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Recipe = () => {
+
+    
+
+    return(
+        <div>
+            <h1>Recipe.js</h1>
+        </div>
+    );
+}
+
+export default Recipe;
