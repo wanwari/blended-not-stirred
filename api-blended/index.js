4d7e36d8c9c2a062dc7e05829b899020278f0295
Wiesa Anwari <contact@wanwari.ca>
2020-05-30 15:50:06 -0400

    added mongoose and created recipe schema/model

--- api-blended/index.js
+++ api-blended/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const cors = require('cors')
+const mongoose = require('mongoose');
 
 const app = express();
-
 app.use(cors());
 
-const recipe = {
+const mongoDB = 'mongodb://localhost/recipe_db';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const Schema = mongoose.Schema;
+const recipeSchema = new Schema({
+    name: String,
+    type: String,
+    ingredients: [{name: String, amount: String, amountType: String}],
+    categories: [String]
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+const mango = new Recipe({
     name: "Tropical Smoothie",
     type: "SMOOTHIE",
     ingredients: [
@@ -16,26 +32,13 @@ const recipe = {
         {name: "frozen blueberries", amount: "1/2", amountType: "cup"}
     ],
     categories: ["low_fat", "nut free"]
-};
-
-const recipe2 = {
-    name: "Grapefruit ",
-    type: "SMOOTHIE",
-    ingredients: [
-        {name: "grapefruit", amount: "2", amountType: "whole"},
-        {name: "sugar", amount: "3", amountType: "tablespoons"},
-        {name: "ice", amount: "1", amountType: "cup"},
-    ],
-    categories: ["low_fat", "nut free"]
-};
+});
 
 app.get('/get_recipe/tropical', (req, res) => {
-    res.send(recipe);
+    res.send(mango);
 });
 
-app.get('/get_recipe/grapefruit', (req, res) => {
-    res.send(recipe2);
-});
+
 
 const server = app.listen(8181, () => {
     console.log("Listening at ", server.address().port);
