f4b34e803d5062e0ca5e0ae69e4d54ef87875a47
Wiesa Anwari <contact@wanwari.ca>
2020-05-29 22:36:48 -0400

    created basic api

---
+++ api-blended/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors());
+
+const recipe = {
+    name: "Tropical Smoothie",
+    type: "SMOOTHIE",
+    ingredients: [
+        {name: "mango", amount: "1", amountType: "diced"},
+        {name: "frozen strawberries", amount: "1", amountType: "cup"},
+        {name: "low-fat vanilla yogurt", amount: "1", amountType: "cup"},
+        {name: "pineapple", amount: "6", amountType: "ounches"},
+        {name: "frozen blueberries", amount: "1/2", amountType: "cup"}
+    ],
+    categories: ["low_fat", "nut free"]
+};
+
+app.get('/get_recipe', (req, res) => {
+    res.send(recipe);
+});
+
+const server = app.listen(8181, () => {
+    console.log("Listening at ", server.address().port);
+});
