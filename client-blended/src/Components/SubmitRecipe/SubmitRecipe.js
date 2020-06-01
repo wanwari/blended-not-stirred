ab6efd711f169196309e0231a25c20cef760a025
Wiesa Anwari <contact@wanwari.ca>
2020-06-01 13:05:30 -0400

    refactored SubmitRecipe

--- client-blended/src/Components/SubmitRecipe/SubmitRecipe.js
+++ client-blended/src/Components/SubmitRecipe/SubmitRecipe.js
@@ -2,24 +2,23 @@ import React, { useState } from 'react';
 
 const SubmitRecipe = () => {
 
-    const [name, setName] = useState("");
-    const [type, setType] = useState("");
+    const [recipeName, setRecipeName] = useState("");
+    const [recipeType, setRecipeType] = useState("");
 
     const [categoriesInput, setCategoriesInput] = useState([""]);
-    const [categories, setCategories] = useState([]);
-
     const [ingredientsInput, setIngredientsInput] = useState([""]);
 
-    const [ingredientName, setIngredientName] = useState([{}, {}]);
+    const [categories, setCategories] = useState([]);
+    const [ingredientName, setIngredientName] = useState([]);
     const [ingredientQuantity, setIngredientQuantity] = useState([]);
     const [ingredientQuantityType, setIngredientQuantityType] = useState([]);
 
-    const handleNameChange = (event) => {
-        setName(event.target.value);
+    const handleRecipeNameChange = (event) => {
+        setRecipeName(event.target.value);
     }
 
-    const handleTypeChange = (event) => {  
-        setType(event.target.value);
+    const handleRecipeTypeChange = (event) => {  
+        setRecipeType(event.target.value);
     }
 
     const handleCategoriesChange = (index, event) => {
@@ -28,75 +27,84 @@ const SubmitRecipe = () => {
         setCategories(newArr);
     }
 
-    const addNewCategories = () => {  
-        setCategoriesInput(prevCategories => [...prevCategories, ""]);
+    const addNewInput = (type) => {  
+        switch (type) {
+            case "CATEGORIES":
+                setCategoriesInput(prevCategories => [...prevCategories, ""]);
+                break;
+            case "INGREDIENT": 
+                setIngredientsInput(prevName => [...prevName, ""]);
+                break;
+            default:
+                console.log("Err: unhandled param passed");
+                break;
+        }
     }
 
-    const handleIngredientNameChange = (index, event) => {
-        let newArr = [...ingredientName];
-        newArr[index] = event.target.value;
-        setIngredientName(newArr);
-    }
-
-    const handleIngredientQuantityChange = (index, event) => {
-        let newArr = [...ingredientQuantity];
-        newArr[index] = event.target.value;
-        setIngredientQuantity(newArr);
-    }
-
-    const handleIngredientQuantityTypeChange = (index, event) => {
-        let newArr = [...ingredientQuantityType];
-        newArr[index] = event.target.value;
-        setIngredientQuantityType(newArr);
-    }
-
-    const addNewIngredient = () => {  
-        setIngredientsInput(prevName => [...prevName, ""]);
+    const handleIngredientChange = (index, event, type) => {
+        let tmpArray;
+        switch (type) {
+            case "NAME":
+                tmpArray = [...ingredientName];
+                tmpArray[index] = event.target.value;
+                setIngredientName(tmpArray);
+                break;
+            case "QUANTITY":
+                tmpArray = [...ingredientQuantity];
+                tmpArray[index] = event.target.value;
+                setIngredientQuantity(tmpArray);
+                break;
+            case "QUANTITYTYPE":
+                tmpArray = [...ingredientQuantityType];
+                tmpArray[index] = event.target.value;
+                setIngredientQuantityType(tmpArray);
+                break;
+            default:
+                console.log("Err: unhandled param passed");
+                break;
+        }
     }
 
     const handleSubmit = () => {
-        console.log("Categories: " + categories);
-        console.log("Name: " + ingredientName);
-        console.log("Quantity: " + ingredientQuantity);
-        console.log("QuantityType: " + ingredientQuantityType);
+        console.log(recipeName);
+        console.log(recipeType);
+        console.log(categories);
+        console.log(ingredientName);
+        console.log(ingredientQuantity);
+        console.log(ingredientQuantityType);
     }
 
     return(
         <div>
             <form>
                 <label htmlFor="recipeName">Name: </label>
-                <input id="recipeName" type="text" value={ name } onChange={ handleNameChange } />  <br></br><br></br>
+                <input id="recipeName" type="text" value={ recipeName } onChange={ handleRecipeNameChange } />  <br></br><br></br>
 
                 <label htmlFor="recipeType">Type: </label>
-                <input id="type" type="text" value={ type } onChange={ handleTypeChange } />
+                <input id="type" type="text" value={ recipeType } onChange={ handleRecipeTypeChange } />
 
                 <h3>Categories</h3>
                 {categoriesInput.map((cat, index) => (
                     <input key={index} onChange={ (event) => handleCategoriesChange(index, event) } type="text" />
                 ))}
 
-                <input type="button" onClick={ addNewCategories } value="Add Category" />
+                <input type="button" onClick={ () => addNewInput("CATEGORIES") } value="Add Category" />
                 
                 <h3>Ingredients</h3>
-
                 {ingredientsInput.map((ing, index) => (
                     <div>
-                        <input key={index} onChange={ (event) => handleIngredientNameChange(index, event) } type="text" />
-                        <input key={index} onChange={ (event) => handleIngredientQuantityChange(index, event) } type="text" />
-                        <input key={index} onChange={ (event) => handleIngredientQuantityTypeChange(index, event) } type="text" />
+                        <input key={index} onChange={ (event) => handleIngredientChange(index, event, "NAME") } type="text" />
+                        <input key={index} onChange={ (event) => handleIngredientChange(index, event, "QUANTITY") } type="text" />
+                        <input key={index} onChange={ (event) => handleIngredientChange(index, event, "QUANTITYTYPE") } type="text" />
                     </div>
-                 
                 ))}
 
-                <input type="button" onClick={ addNewIngredient } value="Add Ingredient" />
+                <input type="button" onClick={ () => addNewInput("INGREDIENT") } value="Add Ingredient" />
 
                 <div>
                     <input type="button" onClick={ handleSubmit } value="Submit" />
                 </div>
-                
-
             </form>
-
         </div>
     );
 }
