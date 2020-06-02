da154a63253c8ac8bbead544cae073ace9c12d88
Wiesa Anwari <contact@wanwari.ca>
2020-06-01 23:43:39 -0400

    refactored MakePost

--- client-blended/src/Components/SubmitRecipe/MakePost.js
+++ client-blended/src/Components/SubmitRecipe/MakePost.js
@@ -1,18 +1,27 @@
-const MakePost = (dataToPost) => {
-    fetch('http://localhost:8181/submit_recipe', {
+const postData = async (url, data) => {
+
+    fetch(url, {
         method: 'POST',
+        cache: 'no-store',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
         },
-            body: JSON.stringify({
-                name: dataToPost.name,
-                type: dataToPost.type,
-                ingredients: dataToPost.ingredients,
-                categories: dataToPost.categories
-            })
-        }).then(console.log("SENT REQUEST"));
+            body: data
+        });
+
+}
+
+const MakePost = (dataToPost) => {
+
+        postData('http://localhost:8181/submit_recipe',
+        JSON.stringify({
+            name: dataToPost.name,
+            type: dataToPost.type,
+            ingredients: dataToPost.ingredients,
+            categories: dataToPost.categories
+        }));
 }
 
 export default MakePost;
