4c813c8c74a8ca59516585de89540b0efb12b4e6
Wiesa Anwari <contact@wanwari.ca>
2020-06-03 22:37:27 -0400

    implemented proper RESTful routes to CRUD mapping

--- api-blended/index.js
+++ api-blended/index.js
@@ -14,55 +14,42 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//import the Recipe mongoose model
-var Recipe = require('./models/Recipe')
+//import the Recipes mongoose model
+var Recipes = require('./models/Recipes')
 
-app.get('/get_recipe/:id', (req, res) => {
-
-    /* TODO
-    * write search algorithm to find and return an array of 
-    * recipies based on search text located in req.params.id
-    */
-    const searchQuery = req.params.id;
-    
-    //currently returns the first recipe with a matching name
-    Recipe.find({name: searchQuery}, function(err, result) { 
-        if(result === undefined || result.length == 0) {
-            res.send(false);
-        } else {
-            res.send(result);
+//GET /recipies
+app.get('/recipies', (req, res) => {
+    console.log('[index.js] GET request made at /recipies');
+    Recipes.find({}, (err, results) => {
+        if (!err) {
+            res.send(results)
         }
-     });
-    
+    });
 });
 
-app.post('/submit_recipe/', (req,res) => {
-    //recieve a Recipe as a JSON object located in req.body
-    
-    //create a new mongoose Recipe model with the data
-    console.log(req.body);
-    var recipeToSave = new Recipe(req.body);
-    //save it to the database.
-    recipeToSave.save();
+//GET /recipies/:id
+app.get('/recipies/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('[index.js] GET request made at /recipies/' + id);
+    Recipes.find({name: id}, (err, results) => {
+        if (!err) {
+            res.send(results);
+        }
+    });
 });
 
-app.get('/list_all/', (req,res) => {
-    Recipe.find({}, function(err, result) { 
-        console.log(result);
-        res.send(result);
-     });
+//POST /recipies
+app.post('/recipies', (req, res) => {
+    new Recipes(req.body).save(() => console.log("Saved"));
 });
 
-app.post('/delete_recipe/:id', (req,res) => {
-    console.log("Here");
-    
-    const idToDelete = req.params.id;
-    console.log(idToDelete);
-    
-    Recipe.remove({_id: idToDelete}, function(err, result) { 
-        console.log(result)
-     });
-     
+app.delete('/recipies/:id', (req, res) => {
+    const id = req.params.id;
+    Recipes.deleteOne({_id: id}, (err, results) => {
+        if (!err) {
+            console.log(results);
+        }
+    });
 });
 
 const server = app.listen(8181, () => {
