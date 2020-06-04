0cd677f98274234d20aef4efbb4064001568a2b8
Wiesa Anwari <contact@wanwari.ca>
2020-06-04 10:17:34 -0400

    removed POST testing component

--- client-blended/src/Components/Routing/Routing.js
+++ client-blended/src/Components/Routing/Routing.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Link, Switch, Route } from 'react-router-dom';
 import Search from '../Search/Search';
-import PostTest from '../PostTest/PostTest';
 import AdminPanel from '../AdminPanel/AdminPanel';
 
 const Routing = () => {
@@ -10,16 +9,12 @@ const Routing = () => {
         <div>
             <ul>
                 <li><Link to="/search">search</Link></li>
-                <li><Link to="/posttest">posttest</Link></li>
                 <li><Link to="/adminpanel">Admin Panel</Link></li>
            </ul>
 
             <Switch>
                 <Route path="/search">
                     <Search />
-                </Route>
-                <Route path="/posttest">
-                    <PostTest />
                 </Route>
                 <Route path="/adminpanel">
                     <AdminPanel />
