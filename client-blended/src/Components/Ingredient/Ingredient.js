f57cfbebc7f58b2efb678799c55e14a873ba0334
Wiesa Anwari <contact@wanwari.ca>
2020-06-07 22:44:02 -0400

    fix(client): changed Ingredient keys to be unique among siblings

--- client-blended/src/Components/Ingredient/Ingredient.js
+++ client-blended/src/Components/Ingredient/Ingredient.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Ingredient = props => {
 
-    const ingredientList = props.ingredients.map((ing) => (
-        <li key={ing.name}>
+    const ingredientList = props.ingredients.map((ing, index) => (
+        <li key={index}>
             {ing.amount} {ing.amountType} {ing.name}
         </li>
     ));
